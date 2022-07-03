7cbd967a0befe04dab3db6b6de26e6bb8ce019f3
Ori Zeiri <oriz400@gmail.com>
2022-07-03 19:22:39 +0300

    init new webapi

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Ta9_Assignment;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
